0884d23f4173c74631693e62dc36d0ce339f3c57
sunil maharjan <sunil.maharjan2@metropolia.fi>
2019-05-04 21:22:11 +0545

    final commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const github = new Github;
+const ui = new UI;
+
+//Search input
+const searchUser = document.getElementById('searchUser');
+
+//search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    //get input text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+        //make http call
+        github.getUser(userText)
+            .then(data => {
+                if(data.profile.message !== undefined || data.profile.message === 'Not Found'){
+                    //show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
+                } else {
+                    //show profile
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
+                }
+            })
+    } else {
+        //clear profile
+        ui.clearProfile();
+    }
+});
